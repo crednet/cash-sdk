8e7429f95be0a7771e18bbc8e6a73b5ea778331e
Damilare Anjorin <damilareanjorin1@gmail.com>
2021-10-28 16:45:01 +0100

    update

--- config/cpcash.php
+++ config/cpcash.php
@@ -1,16 +1,16 @@
 <?php
 
 return [
-    'base_url' => 'https://localhost:8000/api/',
+    'base_url' => env('CPCASH_BASEURL', 'https://localhost:8000/api/'),
     'test' => [
-        'secret_key' => '',
+        'secret_key' => env('CPCASH_TEST_SECRET_KEY'),
 
-        'public_key' => '',
+        'public_key' => env('CPCASH_TEST_PUBLIC_KEY'),
     ],
     'live' => [
-        'secret_key' => '',
+        'secret_key' => env('CPCASH_LIVE_SECRET_KEY'),
 
-        'public_key' => '',
+        'public_key' => env('CPCASH_LIVE_PUBLIC_KEY'),
     ],
 
     'prefix' => 'api/cash',
