97744c65803878da563e50991297d128f212499a
Damilare Anjorin <damilareanjorin1@gmail.com>
2021-12-25 14:36:00 +0100

    chore(phpstan): analysis fix

--- src/Http/Controllers/CPCashController.php
+++ src/Http/Controllers/CPCashController.php
@@ -57,7 +57,7 @@ class CPCashController extends Controller
     public function getWalletTransactions($walletId): JsonResponse
     {
         return $this->successResponse(
-            CPCash::getWalletTransactions($walletId, request()->query('page', 1)),
+            CPCash::getWalletTransactions($walletId, request()->query('page')),
             trans('cpcash::wallet.transactions')
         );
     }
